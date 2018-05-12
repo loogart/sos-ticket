9cceae5cdc1149b9c2834c4fb424f9440fbd78d7
Chris Soueidan <chris@loogart.com>
2018-05-12 10:08:22 -0400

    updated freshchat
    - updated js code for all pages
    - updated mobile menu links

--- js/scripts.js
+++ js/scripts.js
@@ -9,26 +9,3 @@ sr.reveal('.circle-badge', {
 sr.reveal('.featured img', {
     duration: 1000
 }, 150);
-
-//freshchat
-window.fcWidget.init({
-    token: "3bf40794-29e0-4ec1-b829-0e2e8f24f014",
-    host: "https://wchat.freshchat.com"
-});
-
-// Make sure fcWidget.init is included before setting these values
-
-// To set unique user id in your system when it is available
-window.fcWidget.setExternalId("john.doe1987");
-
-// To set user name
-window.fcWidget.user.setFirstName("John");
-
-// To set user email
-window.fcWidget.user.setEmail("john.doe@gmail.com");
-
-// To set user properties
-window.fcWidget.user.setProperties({
-    plan: "Estate", // meta property 1
-    status: "Active" // meta property 2
-});
