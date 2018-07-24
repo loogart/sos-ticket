ab13bedff382a5239c33c493349d35b849e08a6a
Chris Soueidan <chris.soueidan@gmail.com>
2018-07-23 22:50:51 -0400

    updated autoplay carousel

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         items: 1,
         loop: true,
-        //        autoplay: true,
+        autoplay: true,
         autoplayTimeout: 3500,
         animateOut: 'fadeOutRight',
         animateIn: 'fadeInLeft',
