cdb37d839d901d15579ad813dc359f42dde4b592
Chris Soueidan <chris@loogart.com>
2019-08-18 18:06:07 -0400

    Update scripts.js

--- 2019/js/scripts.js
+++ 2019/js/scripts.js
@@ -13,21 +13,21 @@ var nextButton2 = $('#next2');
 
 $("input[type=radio]").change(function () {
     if (vitesse1.is(':checked')) {
-        nextButton.attr("href", "/forfait-1-3-points.html");
+        nextButton.attr("href", "./forfait-1-3-points.html");
     } else if (vitesse2.is(':checked')) {
-        nextButton.attr("href", "/forfait-4-5-points.html");
+        nextButton.attr("href", "./forfait-4-5-points.html");
     } else if (vitesse3.is(':checked')) {
-        nextButton.attr("href", "/forfait-6-points-et-plus.html");
+        nextButton.attr("href", "./forfait-6-points-et-plus.html");
     } else if (vitesseOui.is(':checked')) {
-        nextButton.attr("href", "/forfait-gev.html");
+        nextButton.attr("href", "./forfait-gev.html");
     } else if (autre1.is(':checked')) {
-        nextButton2.attr("href", "/forfait-1-3-points.html");
+        nextButton2.attr("href", "./forfait-1-3-points.html");
     } else if (autre2.is(':checked')) {
-        nextButton2.attr("href", "/forfait-4-5-points.html");
+        nextButton2.attr("href", "./forfait-4-5-points.html");
     } else if (autre3.is(':checked')) {
-        nextButton2.attr("href", "/forfait-6-points-et-plus.html");
+        nextButton2.attr("href", "./forfait-6-points-et-plus.html");
     } else if (autreOui.is(':checked')) {
-        nextButton2.attr("href", "/forfait-facultes-affaiblies.html");
+        nextButton2.attr("href", "./forfait-facultes-affaiblies.html");
     } 
 });
 
