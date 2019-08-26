e446aeacce0d3ffc0c1f46fecd999a4e87f14697
Chris Soueidan <chris.soueidan@gmail.com>
2019-08-25 22:44:36 -0400

    fixed wizard and typos
    - fixed typo in contact page
    - fixed wizard logic jquery
    - fixed vehicules lourds contact button instead of buy

--- 2019/js/scripts.js
+++ 2019/js/scripts.js
@@ -4,6 +4,7 @@ var vitesse3 = $('#vitesse3');
 var vitesseNon = $('#vitesseNon');
 var vitesseOui = $('#vitesseOui');
 var nextButton = $('#next1');
+
 var autre1 = $('#autre1');
 var autre2 = $('#autre2');
 var autre3 = $('#autre3');
@@ -12,22 +13,31 @@ var autreOui = $('#autreOui');
 var nextButton2 = $('#next2');
 
 $("input[type=radio]").change(function () {
-    if (vitesse1.is(':checked')) {
+    //modal 1
+    if ((vitesse1.is(':checked')) && (vitesseNon.is(':checked'))) {
         nextButton.attr("href", "./forfait-1-3-points.html");
-    } else if (vitesse2.is(':checked')) {
+    } else if ((vitesse2.is(':checked')) && (vitesseNon.is(':checked'))) {
         nextButton.attr("href", "./forfait-4-5-points.html");
-    } else if (vitesse3.is(':checked')) {
+    } else if ((vitesse3.is(':checked')) && (vitesseNon.is(':checked'))) {
         nextButton.attr("href", "./forfait-6-points-et-plus.html");
-    } else if (vitesseOui.is(':checked')) {
+    } else if ((vitesse1.is(':checked')) && (vitesseOui.is(':checked'))) {
         nextButton.attr("href", "./forfait-gev.html");
-    } else if (autre1.is(':checked')) {
+    } else if ((vitesse2.is(':checked')) && (vitesseOui.is(':checked'))) {
+        nextButton.attr("href", "./forfait-gev.html");
+    } else if ((vitesse3.is(':checked')) && (vitesseOui.is(':checked'))) {
+        nextButton.attr("href", "./forfait-gev.html");
+        //modal 2
+    } else if ((autre1.is(':checked')) && (autreNon.is(':checked'))) {
         nextButton2.attr("href", "./forfait-1-3-points.html");
-    } else if (autre2.is(':checked')) {
+    } else if ((autre2.is(':checked')) && (autreNon.is(':checked'))) {
         nextButton2.attr("href", "./forfait-4-5-points.html");
-    } else if (autre3.is(':checked')) {
+    } else if ((autre3.is(':checked')) && (autreNon.is(':checked'))) {
         nextButton2.attr("href", "./forfait-6-points-et-plus.html");
-    } else if (autreOui.is(':checked')) {
+    } else if ((autre1.is(':checked')) && (autreOui.is(':checked'))) {
         nextButton2.attr("href", "./forfait-facultes-affaiblies.html");
-    } 
+    } else if ((autre2.is(':checked')) && (autreOui.is(':checked'))) {
+        nextButton2.attr("href", "./forfait-facultes-affaiblies.html");
+    } else if ((autre3.is(':checked')) && (autreOui.is(':checked'))) {
+        nextButton2.attr("href", "./forfait-facultes-affaiblies.html");
+    }
 });
-
