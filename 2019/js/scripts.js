2bd97e38203252bb383b17fd61e9cf97ed64c2c5
matbalazsi <matthew.balazsi@gmail.com>
2019-10-18 14:03:03 -0400

    added english version and merge conflicts

--- 2019/js/scripts.js
+++ 2019/js/scripts.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     // all custom jQuery will go here
 
@@ -33,7 +34,7 @@ $(document).ready(function() {
 
       if(data['speedingFormExcessSpeeding']==='true')
       {
-        window.location.href = '/forfait-droit-criminel';
+        window.location.href = '/forfait-gev';
         return;
       }
 
@@ -67,5 +68,49 @@ $(document).ready(function() {
       submitOtherInfractionForm();
     });
 
+
+var vitesse1 = $('#vitesse1');
+var vitesse2 = $('#vitesse2');
+var vitesse3 = $('#vitesse3');
+var vitesseNon = $('#vitesseNon');
+var vitesseOui = $('#vitesseOui');
+var nextButton = $('#next1');
+
+var autre1 = $('#autre1');
+var autre2 = $('#autre2');
+var autre3 = $('#autre3');
+var autreNon = $('#autreNon');
+var autreOui = $('#autreOui');
+var nextButton2 = $('#next2');
+
+$("input[type=radio]").change(function () {
+    //modal 1
+    if ((vitesse1.is(':checked')) && (vitesseNon.is(':checked'))) {
+        nextButton.attr("href", "./forfait-1-3-points.html");
+    } else if ((vitesse2.is(':checked')) && (vitesseNon.is(':checked'))) {
+        nextButton.attr("href", "./forfait-4-5-points.html");
+    } else if ((vitesse3.is(':checked')) && (vitesseNon.is(':checked'))) {
+        nextButton.attr("href", "./forfait-6-points-et-plus.html");
+    } else if ((vitesse1.is(':checked')) && (vitesseOui.is(':checked'))) {
+        nextButton.attr("href", "./forfait-gev.html");
+    } else if ((vitesse2.is(':checked')) && (vitesseOui.is(':checked'))) {
+        nextButton.attr("href", "./forfait-gev.html");
+    } else if ((vitesse3.is(':checked')) && (vitesseOui.is(':checked'))) {
+        nextButton.attr("href", "./forfait-gev.html");
+        //modal 2
+    } else if ((autre1.is(':checked')) && (autreNon.is(':checked'))) {
+        nextButton2.attr("href", "./forfait-1-3-points.html");
+    } else if ((autre2.is(':checked')) && (autreNon.is(':checked'))) {
+        nextButton2.attr("href", "./forfait-4-5-points.html");
+    } else if ((autre3.is(':checked')) && (autreNon.is(':checked'))) {
+        nextButton2.attr("href", "./forfait-6-points-et-plus.html");
+    } else if ((autre1.is(':checked')) && (autreOui.is(':checked'))) {
+        nextButton2.attr("href", "./forfait-facultes-affaiblies.html");
+    } else if ((autre2.is(':checked')) && (autreOui.is(':checked'))) {
+        nextButton2.attr("href", "./forfait-facultes-affaiblies.html");
+    } else if ((autre3.is(':checked')) && (autreOui.is(':checked'))) {
+        nextButton2.attr("href", "./forfait-facultes-affaiblies.html");
+    }
+});
 
 });
