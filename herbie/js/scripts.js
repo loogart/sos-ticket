3224e9929c1515ae6190d9e6f2cfe7337ce99c6a
Chris Soueidan <chris@loogart.com>
2019-12-12 20:06:28 -0500

    completed full ux and ui pieces

--- herbie/js/scripts.js
+++ herbie/js/scripts.js
@@ -1,5 +1,37 @@
 AOS.init();
+window.addEventListener('load', AOS.refresh);
 // load header and footer
 $(function () {
     $("#herbieHeader").load("../html/herbie_header.html");
+});
+
+// more options appear
+$("#moreButton").click(function () {
+    $(this).addClass("d-none")
+});
+
+// short answer appear
+$("#shortAnswer").click(function () {
+    $(".short-answer").addClass("d-block");
+
+        $('html, body').animate({
+        scrollTop: $(".short-answer").offset().top-100
+    }, 1500);
+});
+
+// range slider
+var slider = document.getElementById("formControlRange");
+var output = document.getElementById("showValue");
+output.innerHTML = slider.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+
+// datepicker customization
+$('.datepicker').datepicker({
+    format: 'dd/mm/yyyy',
+    language: 'fr',
+    orientation: 'bottom',
+    autoclose: true
 });
